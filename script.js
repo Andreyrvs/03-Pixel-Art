de6bb987c252110c8eb87fc13d78f4fdcf053eb3
Andreyrvs <cortaesai@gmail.com>
2021-10-25 16:11:20 -0300

    Requisito - 03

--- script.js
+++ script.js
@@ -2,16 +2,3 @@
 window.onload = () => {
 
 };
-
-function insertColor() {
-  const colors = document.getElementById('color-palette')
-  
-  // for (let index = 0; index < colors.length; index += 1) {
-  //   const element = colors[index];
-    
-  //   element.style.backgroundColor = 'black'
-  //   console.log(element);
-  // }
-  
-}
-insertColor();
