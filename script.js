ab4aa2363ffd1c0af0c4a5ffa1ff33b1a00942b9
Andreyrvs <cortaesai@gmail.com>
2021-10-25 15:58:37 -0300

    Requisito - 02

---
+++ script.js
@@ -0,0 +1,17 @@
+// Exercicio 02 ;
+window.onload = () => {
+
+};
+
+function insertColor() {
+  const colors = document.getElementById('color-palette')
+  
+  // for (let index = 0; index < colors.length; index += 1) {
+  //   const element = colors[index];
+    
+  //   element.style.backgroundColor = 'black'
+  //   console.log(element);
+  // }
+  
+}
+insertColor();
