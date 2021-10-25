f8423b1f52f533033da9c8f3d9cfdd4dac4b77a0
Andreyrvs <cortaesai@gmail.com>
2021-10-25 17:28:21 -0300

    Requisito - 04

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Exercicio 02 ;
-window.onload = () => {
-  createPixelGrid();
-};
-
 function createPixelGrid() {
   const catchBoard = document.getElementById('pixel-board');
-  let n = 24;
-  for (let index = 0; index <= n; index += 1) {
+  const gridNumber = 25;
+  for (let index = 1; index <= gridNumber; index += 1) {
     const createItem = document.createElement('div');
     createItem.className = 'pixel';
-    createItem.style.backgroundColor = 'white';
+    createItem.style.backgroundColor = 'rgb(255,255,255)';
     catchBoard.appendChild(createItem);
   }
 }
+window.onload = () => {
+  createPixelGrid();
+};
