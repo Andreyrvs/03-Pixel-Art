5fb7d3738f3b2416e486d46fdf3fcf53d73e3723
Andreyrvs <cortaesai@gmail.com>
2021-10-25 19:40:49 -0300

    Requisito - 06

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Exercicio 02 ;
 const pixelBoard = document.getElementById('pixel-board');
+const colorPalete = document.getElementById('color-palette');
+
+function addClassColor() {
+  for (let index = 1; index < 5; index += 1) {
+    const createDivColor = document.createElement('div');
+    createDivColor.className = 'color';
+    colorPalete.appendChild(createDivColor);
+  }
+}
 
 function createGrid() {
   for (let index = 0; index < 5; index += 1) {
@@ -16,4 +25,9 @@ function createGrid() {
 
 window.onload = () => {
   createGrid();
+  addClassColor();
 };
+
+colorPalete.addEventListener('click', (e) => {
+  e.target.className = 'selected';
+});
