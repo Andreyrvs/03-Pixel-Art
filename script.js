a2eb2de51cdea0dc5e33d7df975f20e6f6c1f7d6
Andreyrvs <cortaesai@gmail.com>
2021-10-26 11:38:02 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -17,16 +17,33 @@ function createGrid() {
   }
 }
 
-// Requisito 06
+//  seleciona a cor ao clicar na palette.
 
+function catchColor() {
+  const catchClassActiveColor = document.querySelector('.selected');
+  const theCSSprop = window
+    .getComputedStyle(catchClassActiveColor, null)
+    .getPropertyValue('background-color');
+  console.log(theCSSprop);
+  return theCSSprop;
+}
+
+// troca a classe ao clicar na palette;
 colorPalete.addEventListener('click', (e) => {
   if (e.target.id !== 'color-palette') {
     document.querySelector('.selected').classList.remove('selected');
 
     e.target.classList.add('selected');
+    catchColor();
   }
 });
 
+pixelBoard.addEventListener('click', (e) => {
+  if (e.target.id !== 'pixel-board') {
+    e.target.style.backgroundColor = catchColor();
+    // console.log(test);
+  }
+});
 window.onload = () => {
   createGrid();
 };
